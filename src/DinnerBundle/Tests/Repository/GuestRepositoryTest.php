9f60b6b483e7d0f6697127941f1b2a68052610c2
yitznewton <yitznewton@hotmail.com>
2017-10-22 09:46:46 -0400

    Refactor hydration in test

--- src/DinnerBundle/Tests/Repository/GuestRepositoryTest.php
+++ src/DinnerBundle/Tests/Repository/GuestRepositoryTest.php
@@ -4,6 +4,7 @@ use DinnerBundle\Entity\Guest;
 use Doctrine\ORM\Tools\SchemaTool;
 use function iter\map;
 use function iter\toArray;
+use nemesis\Doctrine\Hydrator\ArrayHydrator;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 class GuestRepositoryTest extends KernelTestCase
@@ -14,12 +15,16 @@ class GuestRepositoryTest extends KernelTestCase
     /** @var \DinnerBundle\Repository\GuestRepository */
     private $repository;
 
+    /** @var ArrayHydrator */
+    private $hydrator;
+
     public function setUp()
     {
         self::bootKernel();
 
         $this->em = self::$kernel->getContainer()->get('doctrine')->getManager();
         $this->repository = $this->em->getRepository(Guest::class);
+        $this->hydrator = new ArrayHydrator($this->em);
 
         $metadata = $this->em->getMetadataFactory()->getAllMetadata();
         $schemaTool = new SchemaTool($this->em);
@@ -31,26 +36,22 @@ class GuestRepositoryTest extends KernelTestCase
      */
     public function seated_includes_sorted_comp_and_paid_seated_only()
     {
-        $compSeatedGuest = $this->hydrate(['familyName' => 'Abel', 'compSeats' => 1]);
-        $paidSeatedGuest = $this->hydrate(['familyName' => 'Johnson', 'paidSeats' => 1]);
-        $nonSeatedGuest = $this->hydrate(['familyName' => 'Yterby']);
+        $compSeatedGuest = $this->hydrate(['family_name' => 'Abel', 'comp_seats' => 1]);
+        $paidSeatedGuest = $this->hydrate(['family_name' => 'Johnson', 'paid_seats' => 1]);
+        $nonSeatedGuest = $this->hydrate(['family_name' => 'Yterby']);
 
         $this->em->persist($paidSeatedGuest);
         $this->em->persist($compSeatedGuest);
         $this->em->persist($nonSeatedGuest);
         $this->em->flush();
 
-        $this->assertEquals(['Abel', 'Johnson'], toArray(map(function ($g) { return $g->familyName; }, $this->repository->seated())));
+        $this->assertEquals(['Abel', 'Johnson'], toArray(map(function ($g) {
+            return $g->familyName;
+        }, $this->repository->seated())));
     }
 
     private function hydrate($data)
     {
-        $guest = new Guest();
-
-        foreach ($data as $name => $value) {
-            $guest->$name = $value;
-        }
-
-        return $guest;
+        return $this->hydrator->hydrate(Guest::class, $data);
     }
 }
