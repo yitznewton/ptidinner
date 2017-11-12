7808c100ed328a6119be4927e68e579b7e20c640
yitznewton <yitznewton@hotmail.com>
2017-11-11 23:19:13 -0500

    Update totals

--- src/DinnerBundle/Repository/GuestRepository.php
+++ src/DinnerBundle/Repository/GuestRepository.php
@@ -21,8 +21,8 @@ class GuestRepository extends EntityRepository
     {
         $query = $this->getEntityManager()->createQuery(<<<EOD
             SELECT SUM(g.paid) AS paid,
-            SUM(g.pledge2016) AS pledgeCurrent,
-            SUM(g.pledge2016)-SUM(g.paid) AS balance,
+            SUM(g.pledge2017) AS pledgeCurrent,
+            SUM(g.pledge2017)-SUM(g.paid) AS balance,
             SUM(g.paidSeats) AS paidSeats,
             SUM(g.paidSeats)+SUM(g.compSeats) AS totalSeats
             FROM DinnerBundle\Entity\Guest g
