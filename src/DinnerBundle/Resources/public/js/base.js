b6379af1fce3b1e37c78b768aa0eb315c8926a49
yitznewton <yitznewton@hotmail.com>
2017-11-21 15:35:31 -0500

    Add more auto-dating form controls

--- src/DinnerBundle/Resources/public/js/base.js
+++ src/DinnerBundle/Resources/public/js/base.js
@@ -1,19 +1,38 @@
 (function () {
-    document.querySelector('.confirm-on-submit').addEventListener('submit', function (e) {
-        var response = window.confirm('Are you sure?')
-
-        if (response) {
-            return true;
+    let iterateSelector = function(selector, callback) {
+        var results = document.querySelectorAll(selector);
+        for (var item of results) {
+            callback(item);
         }
+    };
 
-        e.preventDefault();
-        return false;
+    iterateSelector('.confirm-on-submit', function (s) {
+        s.addEventListener('submit', function (e) {
+            var response = window.confirm('Are you sure?');
+
+            if (response) {
+                return true;
+            }
+
+            e.preventDefault();
+            return false;
+        });
     });
 
+    var sentToPrinterInput = document.querySelector('#dinnerbundle_ad_sentToPrinter');
     var approvedCheckbox = document.querySelector('#dinnerbundle_ad_proofApproved');
 
-    document.querySelector('.approve-button').addEventListener('click', function () {
-        approvedCheckbox.checked = true;
-        this.form.submit();
+    iterateSelector('.sent-to-printer-button', function (s) {
+        s.addEventListener('click', function () {
+                sentToPrinterInput.value = (new Date().toISOString().slice(0,10));
+                this.form.submit();
+        });
+    });
+
+    iterateSelector('.approve-button', function (s) {
+        s.addEventListener('click', function () {
+            approvedCheckbox.checked = true;
+            this.form.submit();
+        });
     });
 })();
