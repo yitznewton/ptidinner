d16a11eca7db3c60dba0dadc60aa339325266995
yitznewton <yitznewton@hotmail.com>
2017-12-02 18:52:27 -0500

    Cleanup

--- app/DoctrineMigrations/Version20171202233256_NewFromPrinterProcess.php
+++ app/DoctrineMigrations/Version20171202233256_NewFromPrinterProcess.php
@@ -14,10 +14,6 @@ class Version20171202233256_NewFromPrinterProcess extends AbstractMigration
     {
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
         $this->addSql('ALTER TABLE ads MODIFY proof_from_printer BOOLEAN NOT NULL DEFAULT FALSE');
-//        $this->addSql('ALTER TABLE ads ADD temp_proof_from_printer BOOLEAN NOT NULL DEFAULT FALSE');
-//        $this->addSql('UPDATE ads SET temp_proof_from_printer = TRUE WHERE proof_from_printer IS NOT NULL');
-//        $this->addSql('ALTER TABLE ads DROP proof_from_printer');
-//        $this->addSql('ALTER TABLE ads CHANGE temp_proof_from_printer proof_from_printer BOOLEAN NOT NULL DEFAULT FALSE');
     }
 
     /**
