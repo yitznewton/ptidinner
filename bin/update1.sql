0a6f3bf7653fce11ecd8f0ec2c75bdc993a4b5a1
yitznewton <yitznewton@hotmail.com>
2018-11-07 16:26:42 -0500

    wip to SQL scripts

--- bin/update1.sql
+++ bin/update1.sql
@@ -1,15 +1,15 @@
 update ad_types set type_accession_count=0;
 
 alter table guests
-  add column pledge_2016 float(8,2) not null default 0,
-  drop column pledge_2013
+  add column pledge_2017 float(8,2) not null default 0,
+  drop column pledge_2014
 ;
 
 -- remove PTI, PP, OOT affiliation
-delete from honoree_guest_assoc where honoree_id IN (1, 5, 31);
+-- delete from honoree_guest_assoc where honoree_id IN (1, 5, 31);
 
 update guests
-  set pledge_2016=paid,
+  set pledge_2017=paid,
   pledge_current=0,
   paid=0,
   ad_sum=0,
@@ -20,5 +20,5 @@ update guests
 
 -- add PP affiliation
 insert into honoree_guest_assoc (honoree_id, guest_id)
-    select 5, id from guests where pledge_2014 + pledge_2015 + pledge_2016 > 0
+    select 5, id from guests where pledge_2015 + pledge_2016 + pledge_2017 > 0
     ON DUPLICATE KEY UPDATE honoree_id = 5;
--- bin/update2.sql
+++ bin/update2.sql
@@ -2,7 +2,7 @@
 delete from honorees where id NOT IN (1, 5, 31);
 
 insert into honorees (name, code) values
-    ('Hamburger', 'HAM'),
-    ('Kloor', 'KLR'),
-    ('Novichenok', 'NOV')
+    ('Schaffer', 'SCH'),
+    ('Kreslavskiy', 'KRE'),
+    ('Shulman', 'SHU')
 ;
